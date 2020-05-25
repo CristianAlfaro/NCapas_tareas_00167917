5a522a37706d3ab9f9e5d7236b7871698d572229
CristianAlfaro <00167917@uca.edu.sv>
2020-05-24 19:13:47 -0600

    actividad labo 6 terminada

---
+++ Actividad_Laboratorio6/src/main/java/com/uca/capas/service/EstudianteService.java
@@ -0,0 +1,15 @@
+package com.uca.capas.service;
+
+import java.util.List;
+import org.springframework.dao.DataAccessException;
+import com.uca.capas.domain.Estudiante;
+
+public interface EstudianteService {
+	
+	public List<Estudiante> findAll() throws DataAccessException;
+	
+	public void insert(Estudiante estudiante) throws DataAccessException;
+	
+	public void delete(Integer codigo) throws DataAccessException;
+
+}
